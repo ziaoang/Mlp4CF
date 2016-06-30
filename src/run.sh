1feaa17ad5411b2750a28d9470f358fd560eca66
ziaoang <ziaoang@kdd118.icst>
2016-07-01 01:23:10 +0800

    rm a bug

---
+++ src/run.sh
@@ -0,0 +1,12 @@
+for batch_size in 64 256 1024; do
+    for learn_rate in 0.5 0.1 0.05 0.01; do
+        for re_lambda in 0 0.1 0.01 0.001; do
+            python mf.py ${batch_size} ${batch_size} ${re_lambda} > log/mf_${batch_size}_${batch_size}_${re_lambda}.log
+            python bmf.py ${batch_size} ${batch_size} ${re_lambda} > log/bmf_${batch_size}_${batch_size}_${re_lambda}.log
+            python a_b.py ${batch_size} ${batch_size} ${re_lambda} > log/a_b_${batch_size}_${batch_size}_${re_lambda}.log
+            python a_b_ab.py ${batch_size} ${batch_size} ${re_lambda} > log/a_b_ab_${batch_size}_${batch_size}_${re_lambda}.log
+        done
+    done
+done
+
+
